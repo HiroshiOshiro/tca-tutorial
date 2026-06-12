5b1ab994d0c50b13c0a6b7da245848276e279414
HiroshiOshiro <ooshiro010@gmail.com>
2026-06-12 19:00:51 +0900

    Integrating with SwiftUI

--- tca-tutorial/tca-tutorial/CounterFeature.swift
+++ tca-tutorial/tca-tutorial/CounterFeature.swift
@@ -6,6 +6,7 @@
 //
 
 import ComposableArchitecture
+import SwiftUI
 
 @Reducer
 struct CounterFeature {
@@ -13,25 +14,63 @@ struct CounterFeature {
     struct State {
         var count = 0
     }
-    
+
     enum Action {
         case decrementButtonTapped
         case incrementButtonTapped
     }
 
-    var body: some ReducerOf<Self> {
+    var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
             case .decrementButtonTapped:
                 state.count += 1
-                return .none // state の講師だけの場合は .none
-                
+                return .none  // state の講師だけの場合は .none
+
             case .incrementButtonTapped:
                 state.count -= 1
                 return .none
-                
+
             }
         }
     }
 }
 
+struct CounterView: View {
+    let store: StoreOf<CounterFeature>
+
+    var body: some View {
+        VStack {
+            Text("\(store.count)")
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            HStack {
+                Button("-") {
+                    store.send(.decrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+
+                Button("+") {
+                    store.send(.incrementButtonTapped)
+                }
+                .font(.largeTitle)
+                .padding()
+                .background(Color.black.opacity(0.1))
+                .cornerRadius(10)
+            }
+        }
+    }
+}
+
+#Preview {
+  CounterView(
+    store: Store(initialState: CounterFeature.State()) {
+      CounterFeature()
+    }
+  )
+}
