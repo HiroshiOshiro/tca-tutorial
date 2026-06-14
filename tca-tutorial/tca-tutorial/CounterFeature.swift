99d8b787e15814704365c92476066f6eec415dbb
HiroshiOshiro <ooshiro010@gmail.com>
2026-06-14 18:17:55 +0900

    Testing effects

--- tca-tutorial/tca-tutorial/CounterFeature.swift
+++ tca-tutorial/tca-tutorial/CounterFeature.swift
@@ -29,6 +29,8 @@ struct CounterFeature {
     
     nonisolated enum CancelID: Hashable, Sendable { case timer }
     
+    @Dependency(\.continuousClock) var clock
+    
     var body: some Reducer<State, Action> {
         Reduce { state, action in
             switch action {
@@ -63,8 +65,7 @@ struct CounterFeature {
                 
                 if state.isTimerRunning {
                     return .run { send in
-                        while true {
-                            try await Task.sleep(for: .seconds(1))
+                        for await _ in await self.clock.timer(interval: .seconds(1)) {
                             await send(.timerTick)
                         }
                     }
