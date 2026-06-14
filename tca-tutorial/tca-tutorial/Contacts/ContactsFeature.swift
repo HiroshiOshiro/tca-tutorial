07717f66e325cc00c73833cdc9ad7377ad1bf86f
HiroshiOshiro <ooshiro010@gmail.com>
2026-06-14 21:32:34 +0900

    Child-to-parent communication

--- tca-tutorial/tca-tutorial/Contacts/ContactsFeature.swift
+++ tca-tutorial/tca-tutorial/Contacts/ContactsFeature.swift
@@ -37,16 +37,18 @@ struct ContactsFeature {
                 )
                 return .none
                 
-            case .addContact(.presented(.cancelButtonTapped)):
-                state.addContact = nil
-                return .none
+////            case .addContact(.presented(.cancelButtonTapped)):
+//            case .addContact(.presented(.delegate(.cancel))):
+//                state.addContact = nil
+//                return .none
                 
                 
-            case .addContact(.presented(.saveButtonTapped)):
-                guard let contact = state.addContact?.contact
-                else { return .none }
+//            case .addContact(.presented(.saveButtonTapped)):
+            case let .addContact(.presented(.delegate(.saveContact(contact)))):
+//                guard let contact = state.addContact?.contact
+//                else { return .none }
                 state.contacts.append(contact)
-                state.addContact = nil
+//                state.addContact = nil
                 return .none
                 
             case .addContact:
